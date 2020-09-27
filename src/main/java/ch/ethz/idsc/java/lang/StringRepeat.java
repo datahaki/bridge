7dbde682cc37abcd8786f85425b7f1ff0ac6c201
Jan Hakenberg <jan.hakenberg@gmail.com>
2020-09-27 10:13:42 +0200

    string repeat

--- src/main/java/ch/ethz/idsc/java/lang/StringRepeat.java
+++ src/main/java/ch/ethz/idsc/java/lang/StringRepeat.java
@@ -1,17 +1,18 @@
 // code by jph
 package ch.ethz.idsc.java.lang;
 
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 /** inspired by
  * <a href="https://reference.wolfram.com/language/ref/StringRepeat.html">StringRepeat</a> */
 public enum StringRepeat {
   ;
   /** @param string
-   * @param n
+   * @param n non-negative
    * @return */
   public static String of(String string, int n) {
-    return IntStream.range(0, n).mapToObj(i -> string).collect(Collectors.joining());
+    StringBuilder stringBuilder = new StringBuilder(n * string.length());
+    Stream.generate(() -> string).limit(n).forEach(stringBuilder::append);
+    return stringBuilder.toString();
   }
 }
--- src/test/java/ch/ethz/idsc/java/lang/StringRepeatTest.java
+++ src/test/java/ch/ethz/idsc/java/lang/StringRepeatTest.java
@@ -7,4 +7,14 @@ public class StringRepeatTest extends TestCase {
   public void testSimple() {
     assertEquals(StringRepeat.of("ab", 3), "ababab");
   }
+
+  public void testFailNegative() {
+    assertEquals(StringRepeat.of("ab", 0), "");
+    try {
+      StringRepeat.of("ab", -1);
+      fail();
+    } catch (Exception exception) {
+      // ---
+    }
+  }
 }
