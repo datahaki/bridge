fcbe352dd7838e45a8df366f2816c671b0ff6a37
Jan Hakenberg <jan.hakenberg@gmail.com>
2021-04-08 15:25:38 +0200

    auto sort configurable

--- src/main/java/ch/ethz/idsc/tensor/fig/VisualRow.java
+++ src/main/java/ch/ethz/idsc/tensor/fig/VisualRow.java
@@ -16,13 +16,15 @@ public class VisualRow implements Serializable {
   private final Tensor points;
   private final ComparableLabel comparableLabel;
   private Color color = Color.BLUE;
+  private boolean autoSort = false;
   /** not serializable */
   private transient Stroke stroke;
 
   /** Mathematica::ListPlot[points]
    * 
    * @param points of the form {{x1, y1}, {x2, y2}, ..., {xn, yn}}.
-   * The special case when points == {} is also allowed. */
+   * The special case when points == {} is also allowed.
+   * @param index */
   /* package */ VisualRow(Tensor points, int index) {
     ScalarQ.thenThrow(points);
     this.points = points;
@@ -34,32 +36,53 @@ public class VisualRow implements Serializable {
     return points.unmodifiable();
   }
 
+  /***************************************************/
+  /** @param color */
   public void setColor(Color color) {
     this.color = Objects.requireNonNull(color);
   }
 
+  /** @return */
   public Color getColor() {
     return color;
   }
 
+  /***************************************************/
+  /** @param stroke */
   public void setStroke(Stroke stroke) {
     this.stroke = Objects.requireNonNull(stroke);
   }
 
+  /** @return */
   public Stroke getStroke() {
     return Objects.isNull(stroke) //
         ? STROKE_DEFAULT
         : stroke;
   }
 
+  /***************************************************/
+  /** @param string */
   public void setLabel(String string) {
     comparableLabel.setString(string);
   }
 
+  /** @return */
   public String getLabelString() {
     return getLabel().toString();
   }
 
+  /***************************************************/
+  /** @param autoSort */
+  public void setAutoSort(boolean autoSort) {
+    this.autoSort = autoSort;
+  }
+
+  /** @return */
+  public boolean getAutoSort() {
+    return autoSort;
+  }
+
+  /***************************************************/
   /* package */ ComparableLabel getLabel() {
     return comparableLabel;
   }
--- src/main/java/ch/ethz/idsc/tensor/ref/FieldClip.java
+++ src/main/java/ch/ethz/idsc/tensor/ref/FieldClip.java
@@ -1,16 +1,15 @@
 // code by jph
 package ch.ethz.idsc.tensor.ref;
 
-import static java.lang.annotation.ElementType.FIELD;
-
 import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
-@Target(FIELD)
+@Target(ElementType.FIELD)
 public @interface FieldClip {
   /** @return permitted lower bound */
   String min();
