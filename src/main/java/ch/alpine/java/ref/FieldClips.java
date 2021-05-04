b969d985177f3942f74450bc608102784daebb04
Jan Hakenberg <jan.hakenberg@gmail.com>
2021-05-04 20:39:27 +0200

    field clips test

--- src/main/java/ch/alpine/java/ref/FieldClips.java
+++ src/main/java/ch/alpine/java/ref/FieldClips.java
@@ -17,12 +17,16 @@ import ch.alpine.tensor.sca.N;
    * @throws Exception if parsing of strings to scalars fails
    * @throws Exception if units of min and max are incompatible */
   public static Clip of(FieldClip fieldClip) throws Exception {
-    Scalar min = Scalars.fromString(fieldClip.min());
-    Scalar max = Scalars.fromString(fieldClip.max());
-    /** Example: it was discovered that due to floating point imprecision
-     * 20[L*min^-1] < 20.0[L*min^-1] when values are converted to SI unit "m^3*s^-1"
-     * although equality is expected. As a remedy therefore the smaller/larger of
-     * the exact and numeric value of min/max is taken. */
+    return of( //
+        Scalars.fromString(fieldClip.min()), //
+        Scalars.fromString(fieldClip.max()));
+  }
+
+  /** Example: it was discovered that due to floating point imprecision
+   * 20[L*min^-1] < 20.0[L*min^-1] when values are converted to SI unit "m^3*s^-1"
+   * although equality is expected. As a remedy therefore the smaller/larger of
+   * the exact and numeric value of min/max is taken. */
+  /* package */ static Clip of(Scalar min, Scalar max) throws Exception {
     return Clips.interval( //
         Min.of(UnitSystem.SI().apply(min), UnitSystem.SI().apply(N.DOUBLE.apply(min))), //
         Max.of(UnitSystem.SI().apply(max), UnitSystem.SI().apply(N.DOUBLE.apply(max))));
--- src/test/java/ch/alpine/java/ref/FieldClipsTest.java
+++ src/test/java/ch/alpine/java/ref/FieldClipsTest.java
@@ -5,6 +5,7 @@ import java.lang.reflect.Field;
 
 import ch.alpine.tensor.RealScalar;
 import ch.alpine.tensor.qty.Quantity;
+import ch.alpine.tensor.qty.UnitSystem;
 import ch.alpine.tensor.sca.Clip;
 import junit.framework.TestCase;
 
@@ -23,5 +24,17 @@ public class FieldClipsTest extends TestCase {
     Clip clip = FieldClips.of(fieldClip);
     assertEquals(clip.min(), Quantity.of(2000, "kg*m^2*s^-3"));
     assertEquals(clip.max(), Quantity.of(6000, "kg*m^2*s^-3"));
+  }
+
+  public void testIssue1() throws Exception {
+    Clip clip = FieldClips.of(Quantity.of(0, "L*min^-1"), Quantity.of(20, "L*min^-1"));
+    assertTrue(clip.isInside(UnitSystem.SI().apply(Quantity.of(20, "L*min^-1"))));
+    assertTrue(clip.isInside(UnitSystem.SI().apply(Quantity.of(20.0, "L*min^-1"))));
+  }
+
+  public void testIssue2() throws Exception {
+    Clip clip = FieldClips.of(Quantity.of(0, "L*min^-1"), Quantity.of(20.0, "L*min^-1"));
+    assertTrue(clip.isInside(UnitSystem.SI().apply(Quantity.of(20, "L*min^-1"))));
+    assertTrue(clip.isInside(UnitSystem.SI().apply(Quantity.of(20.0, "L*min^-1"))));
   }
 }
