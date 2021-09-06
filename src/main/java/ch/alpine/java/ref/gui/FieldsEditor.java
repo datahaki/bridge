f7efaec67793c69240409a845b640e14865c7189
datahaki <jan.hakenberg@gmail.com>
2021-09-06 15:55:30 +0200

    field labels

--- src/main/java/ch/alpine/java/ref/gui/FieldsEditor.java
+++ src/main/java/ch/alpine/java/ref/gui/FieldsEditor.java
@@ -21,6 +21,7 @@ import javax.swing.JTextArea;
 import javax.swing.JToolBar;
 
 import ch.alpine.java.ref.FieldLabel;
+import ch.alpine.java.ref.FieldLabels;
 import ch.alpine.java.ref.FieldPanel;
 import ch.alpine.java.ref.FieldWrap;
 import ch.alpine.java.ref.ObjectFieldVisitor;
@@ -112,17 +113,36 @@ public class FieldsEditor implements ObjectFieldVisitor {
   }
 
   // ==================================================
+  /** @param key
+   * @param field
+   * @param index non-null iff field represents array, or list
+   * @return */
   private static String text(String key, Field field, Integer index) {
-    FieldLabel fieldLabel = field.getAnnotation(FieldLabel.class);
-    if (Objects.nonNull(fieldLabel))
-      try {
-        return Objects.isNull(index) //
-            ? fieldLabel.text()
-            // we make the assumption that the user wants to see the counter starting from 1
-            : String.format(fieldLabel.text(), index + 1);
-      } catch (Exception exception) {
-        // ---
+    {
+      FieldLabel fieldLabel = field.getAnnotation(FieldLabel.class);
+      if (Objects.isNull(index)) // base case
+        return Objects.isNull(fieldLabel) //
+            ? key.substring(key.lastIndexOf('.') + 1) // default choice: label text as in java code
+            : fieldLabel.text();
+      // ---
+      // below here, index is guaranteed to be non-null
+      if (Objects.nonNull(fieldLabel))
+        try {
+          // we make the assumption that the user wants to see the counter starting from 1
+          return String.format(fieldLabel.text(), index + 1);
+        } catch (Exception exception) {
+          // in case field label is not suitable for use with String#format
+        }
+    }
+    // ---
+    {
+      FieldLabels fieldLabels = field.getAnnotation(FieldLabels.class);
+      if (Objects.nonNull(fieldLabels)) {
+        String[] text = fieldLabels.text();
+        if (index < text.length)
+          return text[index];
       }
+    }
     return key.substring(key.lastIndexOf('.') + 1);
   }
 
