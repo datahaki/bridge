86665b21760cdcb0395645e62ad8a5eef3617622
datahaki <jan.hakenberg@gmail.com>
2022-03-21 17:56:51 +0100

    test helper

--- src/test/java/ch/alpine/java/io/TestHelper.java
+++ src/test/java/ch/alpine/java/io/TestHelper.java
@@ -17,6 +17,7 @@ enum TestHelper {
       URL url = new URL("http://www.google.com");
       URLConnection urlConnection = url.openConnection();
       urlConnection.connect();
+      urlConnection.getInputStream().close();
       return true;
     } catch (Exception exception) {
       return false;
