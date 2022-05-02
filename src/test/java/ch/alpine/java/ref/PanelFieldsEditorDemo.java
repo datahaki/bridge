723f01816713b897461eb501c415583052936f53
datahaki <jan.hakenberg@gmail.com>
2022-05-02 22:23:28 +0200

    pom

--- src/test/java/ch/alpine/java/ref/PanelFieldsEditorDemo.java
+++ src/test/java/ch/alpine/java/ref/PanelFieldsEditorDemo.java
@@ -5,19 +5,22 @@ import javax.swing.JFrame;
 import javax.swing.WindowConstants;
 
 import ch.alpine.java.ref.util.PanelFieldsEditor;
+import ch.alpine.javax.swing.LookAndFeels;
 
 public enum PanelFieldsEditorDemo {
   ;
   public static void main(String[] args) throws Exception {
     SimpleParam simpleParam = new SimpleParam();
+    simpleParam.lookAndFeels = LookAndFeels.LIGHT;
     simpleParam.lookAndFeels.updateUI();
     // ---
     JFrame jFrame = new JFrame();
     jFrame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
     PanelFieldsEditor panelFieldsEditor = new PanelFieldsEditor(simpleParam);
     panelFieldsEditor.addUniversalListener(() -> {
+      // TODO it's probably a good idea to only invoke the l&f once at the beginning
       try {
-        simpleParam.lookAndFeels.updateUI();
+        // simpleParam.lookAndFeels.updateUI();
       } catch (Exception e) {
         e.printStackTrace();
       }
