85aa807abd1e7425abc93d621f498de08f6fca8a
datahaki <jan.hakenberg@gmail.com>
2022-06-28 23:32:59 +0200

    unicode helper

---
+++ src/main/java/ch/alpine/bridge/lang/UnicodeHelper.java
@@ -0,0 +1,67 @@
+// code by jph
+package ch.alpine.bridge.lang;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import ch.alpine.tensor.Scalar;
+import ch.alpine.tensor.io.ResourceData;
+import ch.alpine.tensor.qty.Unit;
+
+/* package */ enum UnicodeHelper {
+  INSTANCE;
+
+  private final Map<String, String> terminators = new HashMap<>();
+  private final Map<String, String> exponents = new HashMap<>();
+  private final Map<String, String> micros = new HashMap<>();
+
+  private UnicodeHelper() {
+    terminators.put("degC", "\u2103"); // unicode oC
+    terminators.put("EUR", "\u20ac"); // unicode EUR
+    terminators.put("USD", "$"); // unicode EUR
+    terminators.put("K", "\u212a"); // unicode K
+    terminators.put("Ohm", "\u2126"); // unicode Omega
+    terminators.put("kOhm", "k\u2126");
+    terminators.put("MOhm", "M\u2126");
+    // ---
+    exponents.put("1", "");
+    exponents.put("-1", "\u207b\u00b9");
+    exponents.put("2", "\u00b2");
+    exponents.put("-2", "\u207b\u00b2");
+    exponents.put("3", "\u00b3");
+    exponents.put("-3", "\u207b\u00b3");
+    // ---
+    Properties properties = ResourceData.properties("/ch/alpine/tensor/qty/si.properties");
+    Set<String> set = properties.stringPropertyNames();
+    for (String key : set)
+      if (key.startsWith("_")) { // inflator
+        String atom = key.substring(1);
+        String uKey = "u" + atom;
+        if (!properties.contains(uKey))
+          micros.put(uKey, '\u03BC' + terminate(atom));
+      }
+  }
+
+  /** @param map
+   * @return for instance "m*s^-2" */
+  public String toString(Map<String, Scalar> map) {
+    return map.entrySet().stream() //
+        .map(entry -> atomString(entry.getKey()) + exponentString(entry.getValue().toString())) //
+        .collect(Collectors.joining(Unit.JOIN_DELIMITER)); // delimited by '*'
+  }
+
+  private String atomString(String atom) {
+    return micros.getOrDefault(atom, terminate(atom));
+  }
+
+  private String terminate(String atom) {
+    return terminators.getOrDefault(atom, atom);
+  }
+
+  private String exponentString(String string) {
+    return exponents.getOrDefault(string, Unit.POWER_DELIMITER + string);
+  }
+}
---
+++ src/test/java/ch/alpine/bridge/lang/UnicodeHelperTest.java
@@ -0,0 +1,15 @@
+// code by jph
+package ch.alpine.bridge.lang;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import java.lang.reflect.Modifier;
+
+import org.junit.jupiter.api.Test;
+
+class UnicodeHelperTest {
+  @Test
+  void testPackageVisibility() {
+    assertFalse(Modifier.isPublic(UnicodeHelper.class.getModifiers()));
+  }
+}
