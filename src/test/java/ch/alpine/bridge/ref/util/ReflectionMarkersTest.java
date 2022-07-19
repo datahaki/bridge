4a685d6fd58db5059d8c835c145c128f6d26e0f1
datahaki <jan.hakenberg@gmail.com>
2022-07-19 09:04:53 +0200

    flatlaf

--- src/test/java/ch/alpine/bridge/ref/util/ReflectionMarkersTest.java
+++ src/test/java/ch/alpine/bridge/ref/util/ReflectionMarkersTest.java
@@ -2,10 +2,8 @@
 package ch.alpine.bridge.ref.util;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
@@ -27,19 +25,13 @@ class ReflectionMarkersTest {
   @ParameterizedTest
   @MethodSource("provideStrings")
   void testArgStream(String string) {
-    System.out.println(">" + string);
+    assertTrue(string.trim().isEmpty());
   }
 
   private static Stream<Arguments> provideStrings() {
-    // AtomicInteger i = new AtomicInteger();
-    Stream<List<Integer>> flatMap = IntStream.range(0, 3).boxed().flatMap(i -> {
-      return IntStream.range(0, 5).mapToObj(j -> List.of(i, j));
-    });
-    List<List<Integer>> list = flatMap.collect(Collectors.toList());
-    System.out.println(list);
     return Stream.of( //
         Arguments.of(""), //
         Arguments.of("  "), //
-        Arguments.of("not blank"));
+        Arguments.of("     "));
   }
 }
