ca76ec494d2daeea5c7eb0ce4ab68b6760f129df
datahaki <jan.hakenberg@gmail.com>
2022-09-22 12:21:45 +0200

    copy on write linked set

--- src/test/java/demo/tensor/fft/FourierDCTDemo.java
+++ src/test/java/demo/tensor/fft/FourierDCTDemo.java
@@ -9,17 +9,17 @@ import org.jfree.chart.JFreeChart;
 import ch.alpine.bridge.fig.ArrayPlot;
 import ch.alpine.tensor.Tensor;
 import ch.alpine.tensor.ext.HomeDirectory;
-import ch.alpine.tensor.fft.FourierDCTMatrix;
+import ch.alpine.tensor.fft.FourierDCT;
 
-public enum FourierDCTMatrixDemo {
+public enum FourierDCTDemo {
   ;
   public static void main(String[] args) throws IOException {
-    for (FourierDCTMatrix fourierDCTMatrix : FourierDCTMatrix.values()) {
-      Tensor matrix = fourierDCTMatrix.of(32);
+    for (FourierDCT fourierDCT : FourierDCT.values()) {
+      Tensor matrix = fourierDCT.matrix(32);
       // System.out.println(Pretty.of(matrix.map(Round._3)));
       JFreeChart jFreeChart = ArrayPlot.of(matrix);
       ChartUtils.saveChartAsPNG(HomeDirectory.Pictures( //
-          FourierDCTMatrixDemo.class.getSimpleName() + fourierDCTMatrix.ordinal() + ".png"), jFreeChart, //
+          FourierDCTDemo.class.getSimpleName() + fourierDCT.ordinal() + ".png"), jFreeChart, //
           400, 400);
     }
   }
