60bec9b03563ae381ff7a38a6e7a65b603c02dbf
datahaki <jan.hakenberg@gmail.com>
2023-08-09 19:52:52 +0200

    consumers test

--- src/test/java/ch/alpine/bridge/lang/ConsumersTest.java
+++ src/test/java/ch/alpine/bridge/lang/ConsumersTest.java
@@ -1,12 +1,11 @@
+// code by jph
 package ch.alpine.bridge.lang;
-
-import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
 
 class ConsumersTest {
   @Test
   void test() {
-    fail("Not yet implemented");
+    Consumers.empty().accept(null);
   }
 }
