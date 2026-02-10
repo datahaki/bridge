16af9e346ea27090c58b571aea726e94471d0a17
datahaki <jan.hakenberg@gmail.com>
2026-02-10 21:39:19 +0100

    scalable image

--- src/main/java/ch/alpine/bridge/awt/ScalableImage.java
+++ src/main/java/ch/alpine/bridge/awt/ScalableImage.java
@@ -1,11 +1,10 @@
 // code by jph
 package ch.alpine.bridge.awt;
 
-import java.awt.Image;
-import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
 import java.util.Objects;
 
+import ch.alpine.tensor.Scalar;
 import ch.alpine.tensor.Tensor;
 import ch.alpine.tensor.Tensors;
 import ch.alpine.tensor.ext.Cache;
@@ -19,30 +18,35 @@ import ch.alpine.tensor.sca.Round;
  * When printing graphics always use the full resolution image for
  * maximum quality. */
 public class ScalableImage {
-  private final Cache<Tensor, Image> cache = Cache.of(this::compute, 1);
   private final BufferedImage bufferedImage;
+  private final Cache<Tensor, BufferedImage> cache = Cache.of(this::compute, 1);
 
-  /** @param bufferedImage
-   * @param hints typically Image.SCALE_SMOOTH, or Image.SCALE_AREA_AVERAGING */
+  /** @param bufferedImage */
   public ScalableImage(BufferedImage bufferedImage) {
     this.bufferedImage = Objects.requireNonNull(bufferedImage);
   }
 
-  /** @param width
+  /** @param imageResize
+   * @param width
    * @param height
-   * @param interpolationType
-   * @return
-   * @see AffineTransformOp#TYPE_NEAREST_NEIGHBOR
-   * @see AffineTransformOp#TYPE_BILINEAR
-   * @see AffineTransformOp#TYPE_BICUBIC */
-  public Image getScaledInstance(ImageResize imageResize, int width, int height) {
+   * @return */
+  public BufferedImage getScaledInstance(ImageResize imageResize, int width, int height) {
     return cache.apply(Tensors.vector(imageResize.ordinal(), width, height));
   }
 
-  private Image compute(Tensor wht) {
-    int a = Round.intValueExact(wht.Get(0));
-    int w = Round.intValueExact(wht.Get(1));
-    int h = Round.intValueExact(wht.Get(2));
-    return ImageResize.values()[a].of(bufferedImage, w, h);
+  public BufferedImage getScaledInstance(ImageResize imageResize, Scalar factor) {
+    Tensor wh = Tensors.vector(bufferedImage.getWidth(), bufferedImage.getHeight()).multiply(factor);
+    return getScaledInstance(imageResize, //
+        Round.intValueExact(wh.Get(0)), //
+        Round.intValueExact(wh.Get(1)));
+  }
+
+  /** @param owh {ordinal, width, height}
+   * @return */
+  private BufferedImage compute(Tensor owh) {
+    int ordinal = Round.intValueExact(owh.Get(0));
+    int width = Round.intValueExact(owh.Get(1));
+    int height = Round.intValueExact(owh.Get(2));
+    return ImageResize.values()[ordinal].of(bufferedImage, width, height);
   }
 }
