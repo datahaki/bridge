bd6561d4249a9f08b88f9221cbcfdd7ba084734a
datahaki <jan.hakenberg@gmail.com>
2026-02-23 12:24:34 +0100

    manipulate

--- src/main/java/ch/alpine/bridge/pro/Manipulate.java
+++ src/main/java/ch/alpine/bridge/pro/Manipulate.java
@@ -3,6 +3,7 @@ package ch.alpine.bridge.pro;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
+import java.util.Objects;
 import java.util.function.Supplier;
 
 import javax.swing.JFrame;
@@ -13,28 +14,43 @@ import javax.swing.WindowConstants;
 import ch.alpine.bridge.awt.AwtUtil;
 import ch.alpine.bridge.ref.util.PanelFieldsEditor;
 
-public enum Manipulate {
-  ;
+/** inspired by
+ * <a href="https://reference.wolfram.com/language/ref/Manipulate.html">Manipulate</a> */
+public class Manipulate {
   public static JFrame asFrame(Object object, Supplier<Container> function) {
-    JFrame jFrame = new JFrame();
+    return new Manipulate(object, function).jFrame;
+  }
+
+  // ---
+  private final JFrame jFrame = new JFrame();
+  private final JPanel jPanel = new JPanel(new BorderLayout());
+  private Container container_prev = null;
+
+  private Manipulate(Object object, Supplier<Container> function) {
     JSplitPane jSplitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
-    final JPanel jPanel = new JPanel(new BorderLayout());
-    jPanel.add(BorderLayout.CENTER, function.get());
+    jPanel.add(BorderLayout.CENTER, container_prev = Objects.requireNonNull(function.get()));
     jSplitPane.setRightComponent(jPanel);
     {
-      JPanel side = new JPanel(new BorderLayout());
-      side.add(BorderLayout.NORTH, AwtUtil.createToolbar(jSplitPane));
+      JPanel editor = new JPanel(new BorderLayout());
+      editor.add(BorderLayout.NORTH, AwtUtil.createToolbar(jSplitPane));
       PanelFieldsEditor panelFieldsEditor = PanelFieldsEditor.nested(object);
-      side.add(BorderLayout.CENTER, panelFieldsEditor.createJScrollPane());
-      jSplitPane.setLeftComponent(side);
-      panelFieldsEditor.addUniversalListener(() -> {
-        jPanel.removeAll();
-        jPanel.add(BorderLayout.CENTER, function.get());
-        jPanel.validate();
-      });
+      editor.add(BorderLayout.CENTER, panelFieldsEditor.createJScrollPane());
+      jSplitPane.setLeftComponent(editor);
+      panelFieldsEditor.addUniversalListener(() -> receive(function.get()));
     }
     jFrame.setContentPane(jSplitPane);
     jFrame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-    return jFrame;
+  }
+
+  private void receive(Container container_next) {
+    Objects.requireNonNull(container_next);
+    if (container_prev == container_next) {
+      container_prev.repaint();
+      // SwingUtilities.invokeLater(geometricComponent.jComponent::repaint);
+    } else {
+      jPanel.removeAll();
+      jPanel.add(BorderLayout.CENTER, container_prev = container_next);
+      jPanel.validate();
+    }
   }
 }
