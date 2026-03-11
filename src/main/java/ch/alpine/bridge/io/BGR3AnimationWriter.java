df44fa308f8fd0e003dba5d7fa7ccabe8528e4f4
datahaki <jan.hakenberg@gmail.com>
2026-03-11 22:01:09 +0100

    bgr3 anim writer

---
+++ src/main/java/ch/alpine/bridge/io/BGR3AnimationWriter.java
@@ -0,0 +1,26 @@
+// code by jph
+package ch.alpine.bridge.io;
+
+import java.awt.image.BufferedImage;
+
+import ch.alpine.tensor.Tensor;
+import ch.alpine.tensor.ext.Jpeg;
+
+/** for use MP4
+ * internally converts images to 3-byte BGR since this is accepted by */
+public record BGR3AnimationWriter(AnimationWriter animationWriter) implements AnimationWriter {
+  @Override // from AnimationWriter
+  public void write(BufferedImage bufferedImage) throws Exception {
+    animationWriter.write(Jpeg.bgr(bufferedImage));
+  }
+
+  @Override // from AnimationWriter
+  public void write(Tensor tensor) throws Exception {
+    animationWriter.write(tensor);
+  }
+
+  @Override // from AnimationWriter
+  public void close() throws Exception {
+    animationWriter.close();
+  }
+}
