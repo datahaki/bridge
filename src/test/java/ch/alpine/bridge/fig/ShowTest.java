72e9c657b10cdf1bf8f7dc27efcb7f77e0acb095
datahaki <jan.hakenberg@gmail.com>
2026-03-16 14:08:00 +0100

    minor

--- src/test/java/ch/alpine/bridge/fig/ShowTest.java
+++ src/test/java/ch/alpine/bridge/fig/ShowTest.java
@@ -3,11 +3,25 @@ package ch.alpine.bridge.fig;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.awt.Dimension;
+
 import org.junit.jupiter.api.Test;
+
+import ch.alpine.bridge.fig.plt.Plot;
+import ch.alpine.tensor.num.Pi;
+import ch.alpine.tensor.sca.Clips;
+import ch.alpine.tensor.sca.tri.Sin;
 
 class ShowTest {
   @Test
   void testFailNull() {
     assertThrows(Exception.class, () -> new Show(null));
+  }
+
+  @Test
+  void testHighResolution() {
+    Show show = new Show();
+    show.add(Plot.of(Sin.FUNCTION, Clips.absolute(Pi.TWO), PlotOption.FILL));
+    show.image(new Dimension(4000, 4000));
   }
 }
