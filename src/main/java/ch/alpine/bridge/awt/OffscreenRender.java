05495c86f418af4a22648d249f1b8126e5f4c2a3
datahaki <jan.hakenberg@gmail.com>
2026-03-26 06:27:20 +0100

    offscreen render

--- src/main/java/ch/alpine/bridge/awt/OffscreenRender.java
+++ src/main/java/ch/alpine/bridge/awt/OffscreenRender.java
@@ -8,17 +8,13 @@ import java.awt.image.BufferedImage;
 
 public enum OffscreenRender {
   ;
-  public static BufferedImage of(Container container, Dimension dimension, int imageType) {
-    IO.println("OffscreenRender=" + dimension);
+  public static BufferedImage of(Container container, int imageType) {
+    Dimension dimension = container.getSize();
     BufferedImage bufferedImage = new BufferedImage(dimension.width, dimension.height, imageType);
     Graphics2D graphics = bufferedImage.createGraphics();
     container.printAll(graphics);
     graphics.dispose();
     return bufferedImage;
-  }
-
-  public static BufferedImage of(Container container, int imageType) {
-    return of(container, container.getSize(), imageType);
   }
 
   public static BufferedImage of(Container container) {
